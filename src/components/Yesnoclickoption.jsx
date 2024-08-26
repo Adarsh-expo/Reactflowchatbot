2aa3197f86154fe6861fc9fb0616c0a6e3638757
adarsh kumar <adarshupadhyay231501@gmail.com>
2024-08-27 04:13:44 +0530

    delete option added at yes no custom node

--- src/components/Yesnoclickoption.jsx
+++ src/components/Yesnoclickoption.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { Handle, Position } from '@xyflow/react';
-function Yesnoclickoption() {
+import { useState } from 'react';
+function Yesnoclickoption({id, data, selected, deletenode}) {
+
+    const [showdelete,setshowdelete]=useState(false)
   return (
     <div className='bg-white w-[14vw] shadow-lg rounded-lg'>
 
-        <div className='bg-[#FF9933] rounded-t-lg w-[100%] h-fit '><i class="ri-arrow-go-back-line text-[2vw] text-white"></i> </div>
+        <div className='bg-[#FF9933] rounded-t-lg w-[100%] flex justify-between h-fit '><i class="ri-arrow-go-back-line text-[2vw] text-white"></i><button onClick={()=>{setshowdelete((pre)=>!pre)}} ><i class="ri-more-2-fill text-black"></i></button>{showdelete && <div  onClick={()=>{deletenode(id)}} className='hover:cursor-pointer text-center rounded-lg bg-white h-[50%] shadow-2xl shadow-black w-[10vw] text-[1.6vw] top-[33%] left-[95%] absolute'>delete</div>} </div>
+        <div>{id}</div>
         <div className='mt-4 ml-2'>Click to edit me!</div>
         <div className='mb-3 mx-auto w-[100%] px-2 mt-20 flex flex-col gap-4   break-words text-blue-500'>
         <Handle type="source" position={Position.Left} id="a" />
